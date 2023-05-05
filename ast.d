c2170ab952a8a17758caf5e87604f20721310019
Minwoo Lee <mlee892@ku.edu>
2023-05-04 19:01:53 -0500

    add r8, r9

--- ast.d
+++
@@ -1,9 +0,0 @@
-ast.o: ast.cpp ast.hpp tokens.hpp position.hpp types.hpp errors.hpp \
- 3ac.hpp symbol_table.hpp
-ast.hpp:
-tokens.hpp:
-position.hpp:
-types.hpp:
-errors.hpp:
-3ac.hpp:
-symbol_table.hpp:
